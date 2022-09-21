02ea2776c293a352900674461f9bbf489639cc2a
rohan <rohan@masterofown.com>
2022-09-21 19:39:20 +0545

    first

---
+++ index.js
@@ -0,0 +1,250 @@
+const jokes = [`Why did the cracker go to the doctor?
+It felt crummy.`,
+`What is cheese that doesn’t belong to you called?
+Nacho cheese.`,`What's black and white and read all over?
+The newspaper.`,`What goes up but never comes down?
+Your age.`,`Why did the scarecrow win an award?
+It was outstanding in the field.`,`What did the left eye say to the right eye?
+Between us, something smells.`,`Who did the ghost take to the dance?
+His ghoul-friend.`,`Why was six afraid of seven?
+Because seven ate nine.`,`Why was it so breezy at the ballgame?
+The stadium was packed with fans.`,`What do you call someone who only passes gas at home?
+A private tutor.`,`How do you stay warm in any room?
+Hang out in the corner — it’s always 90 degrees.`,`Why don't any other shapes talk to circles?
+Because there’s no point.`,`Why did the tomato blush?
+Because it saw salad dressing.`,`Why didn't the pirate shower before he walked the plank?
+Because he’ll just wash up on shore later.`,`Why did the skeleton quit her job?
+Her heart wasn’t in it.`,`What did the little corn say to the mama corn?
+Where is pop corn?`,`What is a tornado's favorite game to play?
+Twister.`,`Where do vampires keep their money?
+A blood blank.`,`Why do bees have sticky hair?Because they use a honeycomb.`,`What did the astronaut say when he crashed into the moon?
+"I Apollo-gize."`,`What dinosaur had the best vocabulary?
+The thesaurus.`,`What do you do if you get peanut butter on your doorknob?
+Use a door jam.`,`What is a tree's favorite beverage?
+Root beer!`,`How do you know the ocean wants to be your friend?
+It waves.`,`What kind of tree fits in your hand?
+A palm tree.`,`Why can’t you tell a window a joke?
+It could crack up.`,`How did the hair stylist beat everyone to the finish line?
+She knew a shortcut.`,`
+What kind of water doesn’t freeze?
+Hot water.`,`Did you hear about the two thieves who stole a calendar?
+They each got six months.`,`What do Olympic sprinters eat before a race?
+Nothing — they fast.`,`What did the drummer name his twin daughters?
+Anna one, Anna two.`,`How did the trash collector do on his first day at work?
+He just picked it up as he went.`,`Why do bowling pins have it so rough?
+They’re always getting knocked down.`,`What did one plate say to the other?
+In case he got a hole in one.`,`Why do melons have weddings?
+The lettuce was a head but the tomato tried to catchup.`,`What do you call an ant who fights crime?
+A vigilante!`,`How much does it cost a pirate to get his ears pierced?
+About a buck an ear.`,`How can you tell a vampire has a cold?
+She starts coffin.`,`Why did the banana go to the doctor?
+Because it wasn't peeling well.`,`
+Why was the mushroom the life of the party?
+It was a fungi`,
+`What do you call a funny mountain?
+Hill-arious.`,`How do pickles enjoy a day out?
+They relish it.`,`What's a pirate's favorite letter?
+Arrrrrrrrr.`,`What does a book do in the winter?
+Puts on a jacket.`,`What kind of haircuts do bees get?
+Buzzzzzcuts.`,`Why was the broom running late?
+It over-swept.`,`What did the flower say after it told a joke?
+I was just pollen your leg.`,`What do you call a boomerang that won’t come back?
+A stick.`,`Why are ghosts bad liars?
+You can see right through them.`,`
+What did one potty say to the other?
+You look a bit flushed.`,`What’s the best way to throw a party on Venus?
+Just planet.`,`What did one volcano say to the other?
+I lava you.`,`What falls but never gets hurt?
+Snow.`,`What do ducks’ tail feathers do?
+Cover their butt-quacks.`,`What’s the difference between a guitar and a fish?
+You can tune a guitar but you can’t tuna fish.`,`What’s a ghost’s favorite drink?
+Ghoul-ade.`,`What room has no doors or windows?
+A mushroom.`,`Does your underwear have holes in it?
+Of course it does — that’s how you get your legs through.`,`Why doesn’t anyone pick Cinderella for the soccer team?
+She always runs away from the ball.`,`
+How do you follow Will Smith in the snow?
+Just look for the fresh prints`,
+`Why did the golfer bring a spare pair of socks?
+Because they cantaloupe.`,`How are cakes like baseball teams?
+They both require a good batter.`,`What building in New York has the most stories?
+The public library.`,`What stays in the corner yet can travel all over the world?
+A stamp.`,`Why are robots never afraid?
+They have nerves of steel.`,`Why did the cabbage win the race?
+Because it was a-head.`,`
+How does a hurricane see?
+With one eye.`,`Did you hear about the kid who drank eight sodas?
+He burped 7-Up.`,`What is blue but not heavy?
+Light blue.`,`Why did the cell phone get glasses?
+Because she lost all her contacts.`,`How did the mobile phone propose to his girlfriend?
+He gave her a ring.`,`What’s the one thing you’re always guaranteed to get on your birthday?
+A year older.`,`What’s the best thing to put into a pie?
+Your teeth.`,
+`How does a cucumber become a pickle?
+It goes through a jarring experience.`,`What’s worse than finding a fly in your soup?
+Finding half a fly.`,`What’s red and bad for your teeth?
+A brick.`,`Why was the weightlifter always annoyed?
+Because he worked with dumbbells.`,`Is your refrigerator running?
+Well then you better catch it before it gets away.`,`What kind of teeth cost one dollar?
+Buck teeth.`,`Can you tell dad jokes if you don’t have any kids?
+Yes, if you’re a faux pa.`,`How do you stop an astronaut’s baby from crying?
+You rocket.`,`What type of candle burns longer?
+None — they all burn shorter.`,`What does a busybody pepper do?
+It gets jalapeño business.`,`When is a door not a door?
+When it’s actually ajar.`,`When is a joke a dad joke?
+When the punchline is a parent.`,`Which is faster? Hot or cold?
+Hot. You can easily catch a cold.`,`What do you think of that new diner on the moon?
+Food was good, but there really wasn't much atmosphere.`,`What do you call two bananas?
+Slippers.`,`Why couldn't the astronaut book a hotel on the moon?
+Because it was full.`,`What day of the week are most twins born on?
+Twos-day!`,`How do you fix a broken tomato?
+With a can of tomato paste.`,`Why did the kids cross the playground?
+To get to the other slide.`,`What did one math book say to the other?
+I have tons of problems.`,`Why did the kid throw a clock out the window?
+To see time fly.`,`Why did the students eat their homework?
+Because the teacher said it was a piece of cake.`,`What time is it when the clock strikes 13?
+Time to get a new clock.`,`What is a computer’s favorite snack?
+Computer chips.`,`What’s a snake’s favorite subject in school?
+Hisss-tory.`,`What's a pirate's favorite subject in school?
+Arrrrrt.`,`What’s a witch’s favorite subject in school?
+Spelling.`,`Where do you go to learn about banana splits?
+Sundae school.`,`Why can’t you trust atoms?
+They make up everything.`,
+`What did the toaster say to the kindergartener?
+Nothing, silly — toasters can’t talk.`,`Why was the fraction worried about marrying the decimal?
+She would have to convert.`,`What's the math teacher's favorite dessert?
+Pi.`,`What did the limestone say to the geologist?
+Don't take me for granite!`,`How does a scientist fresher her breath?
+With experi-ments.`,`What musical instrument is found in the bathroom?
+A tuba toothpaste.`,`Why did the kid bring a ladder to school?
+Because she wanted to go to school.`,`What do elves learn in school?
+The elf-abet.`,`Where do pencils go on vacation?
+Pencil-vania.`,`Why was the computer cold?
+It has a virus.`,`Why did the teacher wear sunglasses to school?
+Because her students were so bright.`,`What did the paper say to the pencil?
+Write on!`,`Why isn't there a clock in the library?
+Because it tocks too much.`,`What did the lunchbox say to the banana?
+You really have appeal.`,`Can a kangaroo jump higher than a house?
+Yes — houses can’t jump.`,`Why don't teddy bears get hungry?
+They’re always stuffed.`,`What’s a cow’s favorite pastime?
+Going to the moooovies.`,`Why was the baby strawberry sad?
+Because its parents were in a jam.`,`Why did the tissue dance?
+It felt the boogie in it.`,`What did one broccoli say to the other?
+It feels like you're stalking me.`,`Why shouldn’t you tell secrets in a cornfield?
+There are ears everywhere.`,`Why isn't your nose 12 inches long?
+Because then it would be a foot.`,`When should you go to the dentist?
+Tooth hurty.`,`Why did the ghost family remodel their house?
+Nobody was using the living room.`,`Why didn’t the dime roll down the hill with the nickel?
+Because it had more cents.`,`What does a vegan zombie eat?
+Graaains.`,`Why didn’t princess Elsa get a balloon?
+She’d only let it go.`,`What do you call a little legume?
+A Tinybean.`,`What do you call a fake noodle?
+An impasta!`,`What does a cloud wear under a raincoat?
+Thunderwear.`,`What do you call a dinosaur fart?
+A blast from the past.`,`How do you talk to a giant?
+Use big words!`,`Which city does Paw Patrol like the most?
+New Yorkie.`,`What do ghosts like to eat in the summer.
+I scream.`,`Why did Superman flush the toilet?
+Because it was his doody.`,`What kind of vegetable is angry?
+A steamed carrot.`,`Where do mermaids look for jobs?
+The kelp-wanted section.`,`What is a scarecrow's favorite fruit?
+A strawberry.`,`Why are strawberries natural musicians?
+They love to jam.`,`Why did the police arrest the chicken on the basketball court?
+Fowl play.`,`Why do you never see blue whales hiding in trees?
+They’re very good at it.`,`How do you stop bulls from charging?
+Cancel their credit cards.`,`Why can’t you trust a zookeeper?
+They all hang out with cheetahs.`,`What do you call a sleeping bull?
+A bull dozer.`,`Where do you find an elephant with no legs?
+Right where you left it.`,`What do you call a team of rabbits walking backwards?
+A receding hare line.`,`What kind of lion doesn't roar?
+A dandelion.`,`What do you call a duck that gets all A's?
+A wise quacker.`,`What is worse than raining cats and dogs?
+Hailing taxis!`,`how to swim?
+The kitty pool.`,`How do you get a squirrel to like you?
+Act like a nut.`,`What do you call two birds in love?
+Tweethearts.`,`What was the first animal in space?
+The cow that jumped over the moon.`,`Why did the dinosaur cross the road?
+Because the chicken wasn't born yet.`,`How do you make an octopus laugh?
+With ten-tickles.`,`Why do shrimp never share?
+They're so shellfish.`,`What do you call a dog magician?
+A labracadabrador.`,`Why aren't dogs good dancers?
+They have two left feet.`,`What happened when the skunk was on trial?
+The judge declared, "Odor in the court, odor in the court!"`,`What do you get when you cross an elephant with a fish?
+Swimming trunks.`,`What do you call a fish without an eye?
+A fsh.`,`What does an evil hen lay?
+Deviled eggs.`,`What do you get if you cross a pie and a snake?
+A pie-thon.`,`Why can't you play hockey with pigs?
+They always hog the puck.`,`Why do porcupines always win the game?
+They have the most points.`,`Where do elephants pack their clothes?
+In their trunks.`,`Where do sheep go on vacation?
+The Baaa-hamas.`,`What time do ducks wake up?
+At the quack of dawn.`,`Why did the giraffes get bad grades?
+She had her head in the clouds?`,`Why didn't the koala bear get the job?
+They said she was over-koala-fied.`,`Who was the owl who did all the tricks?
+Who-dini.`,`What do you call bears with no ears?
+B.`,`What did the mouse say to the keyboard?
+You're my type!`,`How do squids get to school?
+They take an octobus.`,`What do you call a rabbit with lice?
+Bugs Bunny.`,`What did Mama cow say to Baby cow?
+It's pasture bedtime.`,`Why did the turkey join a band?
+So he could use his drumsticks.`,`What do you call a bear with no teeth?
+A gummy bear.`,`What do you call a bear with no teeth?
+A gummy bear.`,`What's black and white and red all over?
+An embarrassed zebra.`,`Why does a seagull fly over the sea?
+Because if it flew over the bay, it would be a baygull.`,`Why couldn't the duck pay the check?
+His bill was too big.`,`Why didn’t the pony sing in the talent show?
+She was a little horse.`,`Knock knock.
+Who’s there?
+Olive.Olive who?
+Olive YOU!`,`Knock knock.
+Who’s there?
+Icy.Icy who?
+Icy you try trying not to laugh at my knock-knock joke?`,`Knock knock!
+Who’s there?
+Boo.Boo who?
+Don’t cry!`,`Knock knock!
+Who’s there?
+Needle.Needle who?
+Needle little help right now.`,`
+Knock knock!
+Who’s there?
+Donut.Donut who?
+Donut ask me, I just go there.`,`
+Knock knock!
+Who’s there?
+Manatee.Manatee who?
+Manatee would be better than a sweater today, it’s hot!`,`
+Knock knock!
+Who’s there?
+Cow.Cow who?
+Cows don’t say who, they say moooo!`,`Knock, knock.
+Who’s there?
+Cash.Cash who?
+No thanks, but I’d love some peanuts.`,`Knock, knock.
+Who’s there?
+Spell.Spell who?
+W.H.O.`,`Knock, knock.
+Who’s there?
+Double.Double who?
+W!`,`Knock, knock.
+Who’s there?
+Lettuce.Lettuce who?
+Lettuce in, it’s cold out here!`,`Knock, knock.
+Who’s there?
+To.
+To who?
+No, it’s to whom!`,`धनले कहिले पनि धनि हुन सक्दैन
+तिम्रेा मन धनि छ भने तिमीलाइ पुग्छ
+धनले कहिले पनि मानिसलाइ पुग्दैन किनभने
+२ दिनकाे जिन्दगिमा मरेर लानु के नै छ र
+`];
+
+
+let jokeShow = document.querySelector(".jokes");
+let displayJokes = ()=>{
+    let num = Math.floor(Math.random()*200);
+    console.log(num)
+    jokeShow.innerText = jokes[num]
+    console.log(jokes[num-1])
+}
+console.log(jokes.length);
+console.log(jokeShow.innerText)
